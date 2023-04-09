d2bf6106abbd6c0e46c86df4b80d42a0c89dff6c
AKSHAY M <88039862+AKSHAYINDIA05@users.noreply.github.com>
2023-04-09 18:39:59 +0000

    Successfully connected the database

--- database.py
+++ database.py
@@ -1,18 +1,74 @@
 import os
 from sqlalchemy import create_engine, text
 
-my_secret = os.environ['connection_db']
+my_secret = os.environ['connection_db_new_user']
 
 engine = create_engine(my_secret,
                        connect_args={"ssl": {
                          "ssl_ca": "/etc/ssl/cert.pem"
                        }})
 
+# def load_jobs_from_db():
+#   with engine.connect() as conn:
+#     result = conn.execute(text("select * from codeasy.jobs"))
+#     jobs = []
+#     for row in result.all():
+#       jobs.append(row._mapping)
+#     return jobs
+
 
 def load_jobs_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text("select * from codeasy.jobs"))
+    result = conn.execute(text("SELECT * FROM jobs"))
     jobs = []
-    for row in result.all():
-      jobs.append(row._mapping)
+    for row in result.fetchall():
+      job = {}
+      for idx, column in enumerate(result.keys()):
+        job[column] = row[idx]
+      jobs.append(job)
     return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id=:job_id"),
+                          {"job_id": id})
+    rows = result.fetchall()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._mapping
+
+
+# def add_application_to_db(job_id, data):
+#   with engine.connect() as conn:
+#     query = text(
+#       "insert into applications (job_id, full_name, number, email, linkedin, education , experience, resume) values (:job_id, :full_name, :number, :email, :linkedin, :education, :experience, :resume)"
+#     )
+#     conn.execute(query,
+#                  job_id=job_id,
+#                  full_name=data['full_name'],
+#                  email=data['email'],
+#                  number=data['number'],
+#                  linkedin=data['linkedin'],
+#                  education=data['education'],
+#                  experience=data['experience'],
+#                  resume=data['resume'])
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text(
+      "insert into applications (job_id, full_name, email, number, linkedin, education , experience, resume) values (:job_id, :full_name, :email, :number,:linkedin, :education, :experience, :resume)"
+    )
+    params = {
+      'job_id': job_id,
+      'full_name': data['full_name'],
+      'email': data['email'],
+      'number': data['number'],
+      'linkedin': data['linkedin'],
+      'education': data['education'],
+      'experience': data['experience'],
+      'resume': data['resume']
+    }
+    conn.execute(query, params)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -14,6 +14,24 @@ def hello_world():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html', job=job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template("application_submitted.html",
+                         application=data,
+                         job=job)
 
 
 if __name__ == '__main__':
